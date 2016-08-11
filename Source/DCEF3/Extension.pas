c52ee520e5927e0500e5b93f71a9f7b5a8610ff7
Walibeiro <Walison@pcjv.de>
2016-08-12 01:05:33 +0200

    Added DCEF3 sub process host

---
+++ Source/DCEF3/Extension.pas
@@ -0,0 +1,56 @@
+unit Extension;
+
+interface
+
+uses
+  ceflib, VCL.Dialogs;
+
+type
+  TCustomRenderProcessHandler = class(TCefRenderProcessHandlerOwn)
+  protected
+    procedure OnWebKitInitialized; override;
+  end;
+
+  TApplicationExtension = class
+    class procedure NewProject;
+    class procedure OpenProject;
+    class procedure LoadProject(const FileName: string);
+  end;
+
+implementation
+
+procedure TCustomRenderProcessHandler.OnWebKitInitialized;
+var
+  ProcessMessage: ICefProcessMessage;
+begin
+  TCefRTTIExtension.Register('HostApplication', TApplicationExtension);
+end;
+
+{ THostExtension }
+
+class procedure TApplicationExtension.LoadProject(const FileName: string);
+var
+  ProcessMessage: ICefProcessMessage;
+begin
+  ProcessMessage := TCefProcessMessageRef.New('LoadProject');
+  ProcessMessage.ArgumentList.SetString(0, FileName);
+  TCefv8ContextRef.Current.Browser.SendProcessMessage(PID_BROWSER, ProcessMessage);
+end;
+
+class procedure TApplicationExtension.NewProject;
+begin
+  TCefv8ContextRef.Current.Browser.SendProcessMessage(PID_BROWSER,
+    TCefProcessMessageRef.New('NewProject'));
+end;
+
+class procedure TApplicationExtension.OpenProject;
+begin
+  TCefv8ContextRef.Current.Browser.SendProcessMessage(PID_BROWSER,
+    TCefProcessMessageRef.New('OpenProject'));
+end;
+
+initialization
+  CefRemoteDebuggingPort := 9000;
+  CefRenderProcessHandler := TCustomRenderProcessHandler.Create;
+  CefBrowserProcessHandler := TCefBrowserProcessHandlerOwn.Create;
+end.
